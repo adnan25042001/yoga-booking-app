ff2ceceafd3a1e329c01bd458c337fdfca9cf419
Adnan <adnan.hussain.136660@gmail.com>
2024-02-24 12:51:56 +0530

    updated delete button

--- frontend/src/components/button/DeleteBtn.tsx
+++ frontend/src/components/button/DeleteBtn.tsx
@@ -1,17 +1,21 @@
-import { Link } from "react-router-dom";
+import { useEffect } from "react";
 
 type Props = {
     id: string;
 };
 
 const DeleteBtn = ({ id }: Props) => {
+    useEffect(() => {
+        console.log(id);
+    }, [id]);
+
     return (
-        <Link
-            to={`/yoga-class/${id}`}
+        <button
+            onClick={() => alert("Delete function will be added soon")}
             className="py-2 px-6 bg-mainClr text-sm xs:text-base text-white font-medium rounded-lg border-2 border-mainClr hover:bg-transparent hover:text-black cursor-pointer"
         >
             Delete
-        </Link>
+        </button>
     );
 };
 
